4383beeb6d0def49474352a9f739c4c2767d1cf2
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-09-02 22:37:22 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Eirik Andresen on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
