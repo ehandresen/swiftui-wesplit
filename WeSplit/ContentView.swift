ddc1e0dc17dfc992e33296783aaf1ec70c45392a
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-09-15 11:35:31 +0200

    , two way binding

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name: String = ""
+    
     var body: some View {
-        NavigationStack { 
+        Form {
+            
+            TextField("Enter your name", text: $name) // $name means two way binding, the value is read, but also written back
+            Text("your name is \(name)")
+        }
+    }
+}
+
+struct MyForm: View {
+    var body: some View {
+        NavigationStack {
             Form {
                 Section {
                     Text("Hello world")
---
+++ WeSplit/State.swift
@@ -0,0 +1,8 @@
+//
+//  State.swift
+//  WeSplit
+//
+//  Created by Eirik Andresen on 02/09/2024.
+//
+
+import Foundation
